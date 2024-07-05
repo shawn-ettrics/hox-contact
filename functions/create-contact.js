61dee9655297146bf204297276f56f3f7c0836cb
shawn-ettrics <88556403+shawn-ettrics@users.noreply.github.com>
2024-07-05 02:41:16 -0700

    option method for preflight

--- functions/create-contact.js
+++ functions/create-contact.js
@@ -1,9 +1,21 @@
 const APOLLO_API_KEY = "F1ylxgjzNqFcVmrxp7XNWQ";
 
 exports.handler = async (event, context) => {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+      body: ''
+    };
+  }
+
   try {
     const fetch = await import('node-fetch').then(mod => mod.default);
-    
+
     const { "first-name": firstName, "last-name": lastName, email, phone, interest, message, label_names } = JSON.parse(event.body);
 
     const response = await fetch('https://api.apollo.io/v1/contacts', {
