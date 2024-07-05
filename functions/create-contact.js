aa6431d1d08b4fab3060a1a27969db481eac80ab
shawn-ettrics <88556403+shawn-ettrics@users.noreply.github.com>
2024-07-05 02:28:54 -0700

    form logic and function update

--- functions/create-contact.js
+++ functions/create-contact.js
@@ -1,33 +1,40 @@
-const fetch = require('node-fetch');
-
-const APOLLO_API_KEY = "F1ylxgjzNqFcVmrxp7XNWQ";
-
 exports.handler = async (event, context) => {
-  const { first_name, last_name, email, phone, interest, message, label_names } = JSON.parse(event.body);
-
-  const response = await fetch('https://api.apollo.io/v1/contacts', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${APOLLO_API_KEY}`
-    },
-    body: JSON.stringify({ 
-      first_name, 
-      last_name, 
-      email, 
-      phone, 
-      custom_fields: {
-        interest,
-        message
-      },
-      label_names 
-    })
-  });
-
-  const data = await response.json();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(data)
+    try {
+      const fetch = await import('node-fetch').then(mod => mod.default);
+      const APOLLO_API_KEY = "F1ylxgjzNqFcVmrxp7XNWQ";
+  
+      const { "first-name": firstName, "last-name": lastName, email, phone, interest, message, label_names } = JSON.parse(event.body);
+  
+      const response = await fetch('https://api.apollo.io/v1/contacts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${APOLLO_API_KEY}`
+        },
+        body: JSON.stringify({
+          first_name: firstName,
+          last_name: lastName,
+          email,
+          phone,
+          custom_fields: {
+            interest,
+            message
+          },
+          label_names
+        })
+      });
+  
+      const data = await response.json();
+  
+      return {
+        statusCode: 200,
+        body: JSON.stringify(data)
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: error.message })
+      };
+    }
   };
-};
+  
