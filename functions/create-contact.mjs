40311f08bd43bf15d84c2b05c13e44f3c1eb119f
shawn-ettrics <88556403+shawn-ettrics@users.noreply.github.com>
2024-07-08 13:37:51 -0700

    using esmodule

---
+++ functions/create-contact.mjs
@@ -0,0 +1,53 @@
+// Change the file name to create-contact.mjs
+
+exports.handler = async function(event, context) {
+    const fetch = await import('node-fetch').then(mod => mod.default);
+  
+    try {
+      const data = JSON.parse(event.body);
+      const contactData = {
+        first_name: data.firstName,
+        last_name: data.lastName,
+        email: data.email,
+        phone: data.phone,
+        organization_name: data.organizationName,
+        label_names: data.label_names,
+        typed_custom_fields: {
+          "668c2a9462dd94078939da01": data.message // Using the retrieved custom field ID
+        }
+      };
+  
+      const response = await fetch('https://api.apollo.io/v1/contacts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Cache-Control': 'no-cache',
+          'X-Api-Key': 'F1ylxgjzNqFcVmrxp7XNWQ'
+        },
+        body: JSON.stringify(contactData)
+      });
+  
+      const result = await response.json();
+  
+      if (response.ok) {
+        console.log('Apollo Response:', result);
+        return {
+          statusCode: 200,
+          body: JSON.stringify({ success: true, message: 'Contact created successfully!' })
+        };
+      } else {
+        console.error('Error creating contact:', result);
+        return {
+          statusCode: response.status,
+          body: JSON.stringify({ success: false, message: result.message || 'Error creating contact' })
+        };
+      }
+    } catch (error) {
+      console.error('Unexpected error:', error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ success: false, message: 'Internal Server Error' })
+      };
+    }
+  };
+  
